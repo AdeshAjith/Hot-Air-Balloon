8fe65480aeb6cf087afdf7a6455e103ec1e2df43
AdeshAjith <75618912+AdeshAjith@users.noreply.github.com>
2021-05-10 16:42:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,21 @@
-var balloon,backgroundImg;
-var mydatabase,balloonPosition;
+var balloon,backgroundImg,balloonAnimation;
+var mydatabase,balloonPositionRef,myposition;
 
 function preload() {
   backgroundImg = loadImage("images/Hot Air Ballon-01.png");
   balloonAnimation = loadAnimation("images/Hot Air Ballon-02.png","images/Hot Air Ballon-03.png","images/Hot Air Ballon-04.png");
-  mydatabase = firebase.database();
-  balloonPosition = mydatabase.ref('Balloon/Position');
-  balloonPosition.on("value",readpos,error);
 }
 
 function setup() {
   createCanvas(500,500);
-  balloon = createSprite(400, 200, 50, 50);
-  balloon.scale = 0.3;
+
+  mydatabase = firebase.database();
+  balloonPositionRef = mydatabase.ref('Balloon/Position');
+  balloonPositionRef.on("value",readpos,error);
+
+  balloon = createSprite(150, 400, 150, 150);
   balloon.addAnimation("Flying",balloonAnimation);
+  balloon.scale = 0.3;
 }
 
 function draw() {
@@ -22,37 +24,34 @@ function draw() {
   textSize(15);
   text("Use the Arrow Keys to move the Hot Air Balloon!",25,50);
 
-  if(keyDown(LEFT_ARROW)){
-    updateHeight(-10,0);
-    balloon.x = balloon.x - 10;
+   if(myposition !== undefined){
+    if(keyDown(LEFT_ARROW)){
+      updateHeight(-10,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      updateHeight(10,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      updateHeight(0,-10);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      updateHeight(0,10);
+    }
   }
-  else if(keyDown(RIGHT_ARROW)){
-    updateHeight(10,0);
-    balloon.x = balloon.x + 10;
-  }
-  else if(keyDown(UP_ARROW)){
-    updateHeight(0,-10);
-    balloon.y = balloon.y - 10;
-  }
-  else if(keyDown(DOWN_ARROW)){
-    updateHeight(0,10);
-    balloon.y = balloon.y + 10;
-  }
-
-  drawSprites();
-}
-
-function readpos(data){
-  mypos = data.val();
-  balloon.x = mypos.x;
-  balloon.y = mypos.y;
+  drawSprites()
 }
 
 function updateHeight(a,b){
   mydatabase.ref('Balloon/Position').set({
-    'x' : mypos.x + a,
-    'y' : mypos.y + b
+    'x' : myposition.x + a,
+    'y' : myposition.y + b
   })
+}
+
+function readpos(mydata){
+  myposition = mydata.val()
+  balloon.x = myposition.x
+  balloon.y = myposition.y
 }
 
 function error(){
